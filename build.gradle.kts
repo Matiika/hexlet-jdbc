68e43228e136edd4c81388743c0f1abbe5da3af9
Матвей <matvei@MacBook-Pro-Matvej.local>
2024-07-30 11:35:46 +0300

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+
+dependencies {
+    implementation("com.h2database:h2:2.2.220")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass = "hexlet.code.Main"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
